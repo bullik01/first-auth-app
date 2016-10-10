82155484fef6e3d431160e59ae885b654dd45b87
bullik01 <xakep.bullik01@gmail.com>
2016-10-10 22:52:16 +0300

    FirstUpd . Added fields pass + mail .

---
+++ app/src/main/java/com/bulo4ka/bullik01/firstauthapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bulo4ka.bullik01.firstauthapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
